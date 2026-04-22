ecbb9bb3eb07e2a9e44b40f25570a2a7c48bc2d9
mushycosma <kevomassawe@gmail.com>
2026-04-22 11:24:47 +0300

    fix:

--- src/services/googleAuth.js
+++ src/services/googleAuth.js
@@ -1,12 +1,21 @@
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from "expo-auth-session";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export const useGoogleAuth = () => {
+
+  const redirectUri = AuthSession.makeRedirectUri({
+    useProxy: true,
+  });
+
   const [request, response, promptAsync] = Google.useAuthRequest({
     expoClientId: "382651467340-nubruhe6cvqh37pqi8fc2725lvk37g16.apps.googleusercontent.com",
     webClientId: "382651467340-nubruhe6cvqh37pqi8fc2725lvk37g16.apps.googleusercontent.com",
+
+    redirectUri,
+    useProxy: true,
   });
 
   return { request, response, promptAsync };
